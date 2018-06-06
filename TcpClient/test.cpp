08ec0f4f23acb334dd42163235980861b4aefb41
SteBonicelli <stebonicelli@gmail.com>
2018-06-06 17:03:49 +0200

    More tests

--- TcpClient/test.cpp
+++ TcpClient/test.cpp
@@ -4,21 +4,16 @@
 
 int main()
 {
-    TcpClient *socket = new TcpClient("127.0.0.1", 8088);
+    TcpClient *socket = new TcpClient("10.0.0.100", 8088);
 
     char c;
-    do {
+
+    do
+    {
         c = getchar();
-        socket->send16(c);
+        socket->send8(c);
     } while (c != '0');
 
     delete socket;
-/*
-    for(uint16_t i = 0; i < 5; i++)
-    {
-    	socket->send16(i);
-        sleep(2);
-    }
-*/
     return 0;
 }
