9c9b7e3dd89eb0f5bd4da2f94ccf24b49808082e
SteBonicelli <stebonicelli@gmail.com>
2018-08-29 14:25:29 +0200

    Automatical update

--- TcpClient/test.cpp
+++ TcpClient/test.cpp
@@ -1,13 +1,21 @@
 #include "TcpClient.h"
 
 #include <unistd.h>
+#include <stdio.h>
 
 int main()
 {
-    TcpClient *socket = new TcpClient("10.0.0.103", 50150);
+    TcpClient *socket1 = new TcpClient("10.0.0.105", 50100);
+    TcpClient *socket2 = new TcpClient("10.0.0.105", 50101);
 
-    socket->send16(0);
-    sleep(1);
+    uint16_t data1, data2;
+
+    while(true)
+    {
+        scanf("%hu", &data1);
+        socket1->send16(data1);
+        socket2->send16(data1);
+    }
 
     delete socket;
     return 0;
